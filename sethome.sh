4109781f6fb1a14fc1e41fd756ea5993c9081649
ihatemodels <gtechno@protonmail.com>
2020-10-10 16:23:15 +0300

    - fix sethome.sh

--- sethome.sh
+++ sethome.sh
@@ -1,36 +1,28 @@
-#!/bib/bash
+#!/bin/bash
 
-RED=`tput bold && tput setaf 1`
-RESET=`tput bold && tput sgr 0`
-BLUE=`tput bold && tput setaf 4`
+sudo apt-get update
+sudo apt-get install vim
+sudo apt-get install git
 
-
-banner() {
-  echo "${BLUE}+--------------------------------+"
-  echo "|                                |"
-  printf "|${RED} %-30s `tput sgr0`${RED}|\n" "$@"
-  echo "+--------------------------------+${RESET}"
-}
-
-banner "making rc backups..."
-## Set rc's
-cp -f ~/.bashrc ~/.bashrc.backup
-cp -f ~/.vimrc ~/.vimrc.backup
-
-banner "copying files..."
-cp -f .bashrc ~/.bashrc
-cp -r .bash/ ~/
-cp -f .gitconfig ~/.gitconfig
-
-if  [ -x "$(command -v vim)" ]; then
-  banner "installing vundle"
-  cp -f .vimrc ~/.vimrc
-  git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-  vim +PluginInstall +qall
-fi
-
-# set dirs
+# Set dirs
 mkdir -p ~/Projects/go
 mkdir -p ~/Scripts
 mkdir -p ~/tools
 mkdir -p ~/configs
+mkdir -p ~/.backups
+
+## Back rc's
+cp -f ~/.bashrc ~/.backups/bashrc.backup
+cp -f ~/.vimrc ~/.backups/vimrc.backup
+
+
+## Set rc's
+cp -f .bashrc ~/.bashrc
+cp -r .bash/ ~/
+cp -f .gitconfig ~/.gitconfig
+
+if [ -x "$(command -v vim)" ]; then
+  cp -f .vimrc ~/.vimrc
+  git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+  vim +PluginInstall +qall
+fi
